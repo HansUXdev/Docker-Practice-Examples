856a93c7470a63b58f27711ac0a888d90d294b71
Hans <bmm79@nau.edu>
2019-07-07 10:03:24 -0700

    update routes

--- LAMP7-laraval/www/photogallery/app/Http/routes.php
+++ LAMP7-laraval/www/photogallery/app/Http/routes.php
@@ -11,6 +11,15 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'GalleryController@index');
+Route::get('/', 'GalleryController@create');
+
+Route::resource('gallery', 'GalleryController');
+Route::resource('photo', 'PhotoController');
+
+
+Route::get('/gallery/show/{id}', 'GalleryController@show');
+ 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
